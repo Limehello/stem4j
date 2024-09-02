60493a9b5bd200bfbbb0f4a4f5fc3cab96bf9d8b
Limehello <plxsmazis@gmail.com>
2024-09-02 20:36:49 +0800

    Update Position.java

--- src/main/java/com/advn/android/stem4j/Science/Physics/Classical Mechanics/Position.java
+++ src/main/java/com/advn/android/stem4j/Science/Physics/Classical Mechanics/Position.java
@@ -1,1 +1,21 @@
+package com.advn.android.stem4j.physics;
 
+public class Position {
+	private double posX, posY;
+	public Position(double x, double y) {
+		posX = x;
+		posY = y;
+	}
+	public double getPosX() {
+		return posX;
+	}
+	public double getPosY() {
+		return posY
+	}
+	public void setPosX(double x) {
+		posX = x;
+	}
+	public void setPosY(double y) {
+		posY = y;
+	}
+}
