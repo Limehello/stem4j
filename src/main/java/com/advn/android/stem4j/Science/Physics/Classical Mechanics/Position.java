97bbdfe4e83c60850734a54b8166027e14b30f10
Limehello <plxsmazis@gmail.com>
2024-09-03 09:56:58 +0800

    Update Position.java

--- src/main/java/com/advn/android/stem4j/Science/Physics/Classical Mechanics/Position.java
+++ src/main/java/com/advn/android/stem4j/Science/Physics/Classical Mechanics/Position.java
@@ -1,74 +1,94 @@
 package com.advn.android.stem4j.physics;
 
-import android.view.View;
+import android.view.*;
 
 public class Position {
-    private double posX, posY;
-
+    private double posX;
+    private double posY;
     public Position(double x, double y) {
         this.posX = x;
         this.posY = y;
     }
-
-    public double getPosX() {
-        return posX;
-    }
-
-    public double getPosY() {
-        return posY;
-    }
-
     public void setPosX(double x) {
         this.posX = x;
     }
-
     public void setPosY(double y) {
         this.posY = y;
     }
-
-    public void movePosX(double deltaX) {
-        this.posX += deltaX;
+    public void setCoords(double x, double y) {
+        this.posX = x;
+        this.posY = y;
     }
-
-    public void movePosY(double deltaY) {
-        this.posY += deltaY;
+    public double getPosX() {
+        return posX;
     }
-
-    public void applyTo(View v) {
-        v.setX((float) posX); // Convert double to float
-        v.setY((float) posY); // Convert double to float
+    public double getPosY() {
+        return posY;
     }
-
+    public void movePosX(double x) {
+        this.posX += x;
+    }
+    public void movePosY(double y) {
+        this.posY += y;
+    }
+    public void adjustCoords(double x, double y) {
+        this.posX += x;
+        this.posY += y;
+    }
+    public void inversePosX() {
+        this.posX = this.posX * -1;
+    }
+    public void inversePosY() {
+        this.posY = this.posY * -1;
+    }
+    public void addPosX(Position other) {
+        this.posX += other.getPosX();
+    }
+    public void addPosY(Position other) {
+        this.posY += other.getPosY();
+    }
     public void resetPosX() {
         this.posX = 0;
     }
-
     public void resetPosY() {
         this.posY = 0;
     }
-    public void resetPos() {
+    public void resetCoords() {
         this.posX = 0;
         this.posY = 0;
     }
-    
-    public static Position fromView(View v) {
+    public void normalize(double minX, double maxX, double minY, double maxY) {
+        if ((maxX - minX) != 0 && (maxY - minY) != 0) {
+            this.posX = (this.posX - minX) / (maxX - minX);
+            this.posY = (this.posY - minY) / (maxY - minY);
+        }
+    }
+    public void rotate(double angleDegrees) {
+     double radians = Math.toRadians(angleDegrees);
+        double newX = this.posX * Math.cos(radians) - this.posY * Math.sin(radians);
+        double newY = this.posX * Math.sin(radians) + this.posY * Math.cos(radians);
+        this.posX = newX;
+        this.posY = newY;
+        }
+    public Position copy() {
+        return new Position(this.posX, this.posY);
+    }
+    public boolean isWithinBounds(double minX, double maxX, double minY, double maxY) {
+        return this.posX >= minX && this.posX <= maxX && this.posY >= minY && this.posY <= maxY;
+    }
+    public Position fromView(View v) {
         return new Position(v.getX(), v.getY());
     }
-
-    public void clampToScreenBounds(View v) {
-        int viewWidth = v.getWidth();
-        int viewHeight = v.getHeight();
-        int screenWidth = v.getRootView().getWidth();
-        int screenHeight = v.getRootView().getHeight();
-
-        if (posX < 0) posX = 0;
-        if (posY < 0) posY = 0;
-        if (posX + viewWidth > screenWidth) posX = screenWidth - viewWidth;
-        if (posY + viewHeight > screenHeight) posY = screenHeight - viewHeight;
+    public void applyTo(View v) {
+        v.setX((float) this.posX);
+        v.setY((float) this.posY);
     }
-
+    
     @Override
     public String toString() {
-        return "posX: " + posX + ", posY: " + posY;
+        return "Position{" +
+               "x=" + posX +
+               ", y=" + posY +
+               "};";
     }
 }
